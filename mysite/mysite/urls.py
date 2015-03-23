8fdf0de5c898709cfb7e85b82acf7975969ef868
nwpuhmz <scuhmz@gmail.com>
2015-03-23 20:59:25 +0800

    part 4-OK

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,5 @@
+from django.conf.urls import patterns, include, url
 
-from django.conf.urls import patterns, url
-from django.views.generic import DetailView, ListView
-from polls.models import Poll
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,23 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    # url(r'^polls/', include('polls.urls',namespace="polls")),
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$',
-        ListView.as_view(
-            queryset=Poll.objects.order_by('-pub_date')[:5],
-            context_object_name='latest_poll_list',
-            template_name='polls/index.html'),
-        name='index'),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/detail.html'),
-        name='detail'),
-    url(r'^(?P<pk>\d+)/results/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/results.html'),
-        name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote', name='vote'),
+     url(r'^polls/', include('polls.urls',namespace="polls")),
+     url(r'^admin/', include(admin.site.urls)),
+   
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,24 +7,24 @@ from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 # Create your views here.
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = Context({
-        'latest_poll_list':latest_poll_list,
-        })
-    return HttpResponse(template.render(context))
+# def index(request):
+#     latest_poll_list = Poll.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = Context({
+#         'latest_poll_list':latest_poll_list,
+#         })
+#     return HttpResponse(template.render(context))
 
-def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-    return render(request, 'polls/detail.html', {'poll': poll})
+# def detail(request, poll_id):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     return render(request, 'polls/detail.html', {'poll': poll})
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll,pk=poll_id)
-    return render(request,'polls/results.html',{'poll':poll})
+# def results(request, poll_id):
+#     poll = get_object_or_404(Poll,pk=poll_id)
+#     return render(request,'polls/results.html',{'poll':poll})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll,pk=poll_id)
